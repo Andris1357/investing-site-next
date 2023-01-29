4011f37a67217a6eb4d0aa1e7f51452fc83d88dd
Andris <Andris1357@users.noreply.github.com>
2023-01-29 21:53:28 +0100

    site main functionality created

--- src/pages/trade.tsx
+++ src/pages/trade.tsx
@@ -1,19 +1,22 @@
 import TradeArea from "@/pages/trade_button_area";
+import RedeemDonationsArea from "./redeem_donations_area";
+import WithdrawArea from "./withdraw_area";
 import {TradeAreaArgs} from "@/pages/trade_button_area";
 
 let available_eth: number = 1.485204;
 let available_tokens: number = 43729;
+let available_donations: number = 73921;
 
 const trade_area_args: TradeAreaArgs[] = [
     {
-        button_id_: "buy-tokens",
-        button_text_: "Buy tokens for ETH" ,
-        info_text_: `Available ETH: ${available_eth}`
+        button_id: "buy-tokens",
+        button_text: "Buy tokens for ETH" ,
+        info_text: `Available ETH: ${available_eth}`
     },
     {
-        button_id_: "buy-eth",
-        button_text_: "Buy ETH for tokens" ,
-        info_text_: `Available tokens: ${available_tokens}`
+        button_id: "buy-eth",
+        button_text: "Buy ETH for tokens" ,
+        info_text: `Available tokens: ${available_tokens}`
     }
 ]
 
@@ -23,14 +26,14 @@ export default function TradingPage(): JSX.Element {
             <div id="trade-area">
                 {trade_area_args.map(object_ => {
                     return <TradeArea 
-                        button_id_={object_.button_id_}
-                        button_text_={object_.button_text_} 
-                        info_text_={object_.info_text_}
+                        button_id={object_.button_id}
+                        button_text={object_.button_text} 
+                        info_text={object_.info_text}
                     />
                 })}
             </div>
-            <div id="redeem-donations-area">Redeem channel donations</div>
-            <div id="withdraw-area">Withdraw tokens to wallet</div>
+            <RedeemDonationsArea available_donations={available_donations}/>
+            <WithdrawArea available_tokens={available_tokens}/>
         </div>
     )
 }
